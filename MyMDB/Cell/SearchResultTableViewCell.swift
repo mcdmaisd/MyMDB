4249cf86c267e12b8f49830f52e279b7f9986c0c
mcdmaisd <tkim0156@gmail.com>
2025-02-01 10:36:23 +0900

    feat: add overviewlabel in search tableview cell

--- MyMDB/Cell/SearchResultTableViewCell.swift
+++ MyMDB/Cell/SearchResultTableViewCell.swift
@@ -13,6 +13,7 @@ final class SearchResultTableViewCell: BaseTableViewCell {
     private let posterView = PosterView()
     private let titleLabel = HeaderLabel()
     private let dateLabel = CustomLabel()
+    private let overviewLabel = OverViewLabel()
     private let likeButton = LikeButton()
     private let scrollView = UIScrollView()
     private let stackView = UIStackView()
@@ -21,6 +22,7 @@ final class SearchResultTableViewCell: BaseTableViewCell {
         addSubView(posterView)
         addSubView(titleLabel)
         addSubView(dateLabel)
+        addSubView(overviewLabel)
         addSubView(likeButton)
         addSubView(scrollView)
         scrollView.addSubview(stackView)
@@ -41,14 +43,21 @@ final class SearchResultTableViewCell: BaseTableViewCell {
         }
         
         dateLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.top.equalTo(titleLabel.snp.bottom)
             make.leading.equalTo(titleLabel)
         }
         
+        overviewLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(5)
+            make.leading.equalTo(titleLabel)
+            make.trailing.equalTo(likeButton)
+            make.bottom.lessThanOrEqualTo(likeButton.snp.top).offset(-5)
+        }
+        
         likeButton.snp.makeConstraints { make in
-            make.trailing.equalTo(safeAreaLayoutGuide).inset(5)
+            make.trailing.equalTo(safeAreaLayoutGuide)
             make.size.equalTo(posterView.snp.height).dividedBy(4)
-            make.bottom.equalTo(posterView)
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom)
         }
         
         scrollView.snp.makeConstraints { make in
@@ -72,6 +81,12 @@ final class SearchResultTableViewCell: BaseTableViewCell {
         
         scrollView.showsHorizontalScrollIndicator = false
         
+        overviewLabel.setContentHuggingPriority(.required, for: .vertical)
+        overviewLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        
+        likeButton.setContentHuggingPriority(.required, for: .vertical)
+        likeButton.setContentCompressionResistancePriority(.required, for: .vertical)
+        
         stackView.axis = .horizontal
         stackView.spacing = 5
         stackView.distribution = .fillProportionally
@@ -82,10 +97,23 @@ final class SearchResultTableViewCell: BaseTableViewCell {
         posterView.configureImageView()
         titleLabel.configureLabel()
         dateLabel.configureLabel()
+        overviewLabel.configureLabel()
         likeButton.configureButton()
         removeSubviews()
     }
-        
+    
+    private func remakeLayout(_ empty: Bool) {
+        likeButton.snp.remakeConstraints { make in
+            make.trailing.equalTo(safeAreaLayoutGuide).inset(5)
+            make.size.equalTo(posterView.snp.height).dividedBy(4)
+            if empty {
+                make.bottom.equalTo(posterView)
+            } else {
+                make.centerY.equalTo(scrollView)
+            }
+        }
+    }
+            
     private func removeSubviews() {
         let subviews = stackView.arrangedSubviews
         
@@ -96,6 +124,7 @@ final class SearchResultTableViewCell: BaseTableViewCell {
     }
     
     private func configureGenreView(_ genres: [Int]) {
+        remakeLayout(genres.isEmpty)
         var genreViews: [GenreView] = []
         
         for genre in genres {
@@ -119,6 +148,7 @@ final class SearchResultTableViewCell: BaseTableViewCell {
         posterView.configureImageView(data.poster_path ?? "")
         titleLabel.configureLabel(data.title, 2)
         dateLabel.configureLabel(data.release_date?.replacingOccurrences(of: AC.dash, with: AC.period) ?? "")
+        overviewLabel.configureLabel(data.overview, 3)
         likeButton.configureButton(data.id)
         configureGenreView(data.genre_ids ?? [])
     }
